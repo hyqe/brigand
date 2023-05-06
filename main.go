d113fe22964178a3e81e7b91b67ca5c5f01bb1e2
LukeOchoa <Luke.Ochoa@gmail.com>
2023-05-05 20:48:50 -0700

    missed files

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
+func store_a_file(w http.ResponseWriter, r *http.Request) {
+
+	b, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	if len(b) < 1 {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]any{
+		"id": uuid.New(),
+	})
+
+}
+
 func main() {
-	// get port
+	router := mux.NewRouter()
+	router.HandleFunc("/files", store_a_file)
+
 	addr := fmt.Sprintf(":%v", os.Getenv("PORT"))
-
 	fmt.Println("Running on ", addr)
-	http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Money")
-	}))
-
+	http.ListenAndServe(addr, router)
 }
