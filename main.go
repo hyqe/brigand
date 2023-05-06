f373ca292a8497736a4111e763287e624d67f672
Hyacinth Hellebores <hyacinth@pop-os.localdomain>
2023-05-05 17:36:56 -0700

    add env arg & money

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
+	addr := fmt.Sprintf(":%v", os.Getenv("PORT"))
 
-	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	fmt.Println("Running on ", addr)
+	http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Money")
+	}))
 
 }
