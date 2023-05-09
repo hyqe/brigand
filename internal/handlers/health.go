992a417e6fe7f028c2a68a1fabc6d0cfc8ed3b47
LukeOchoa <luke.ochoa@gmail.com>
2023-05-09 01:13:20 +0000

    fix test to reliably establish connection & add code to have test insert a document before trying to query for said document

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func NewGetHealth() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		println("I was here")
 		w.WriteHeader(http.StatusOK)
 	}
 }
--- internal/storage/metadata_test.go
+++ internal/storage/metadata_test.go
@@ -8,9 +8,9 @@ import (
 	"github.com/hyqe/brigand/internal/storage"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	// "go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/stretchr/testify/require"
+	"time"
 )
 
 func Test_mongoMetadataClient_GetById_happy_path(t *testing.T) {
@@ -19,17 +19,24 @@ func Test_mongoMetadataClient_GetById_happy_path(t *testing.T) {
 		t.Skipf("missing env: %s", "MONGO")
 	}
 
-	// Get the client
+	// Open Client
 	client, err := mongo.NewClient(options.Client().ApplyURI(MONGO))
 	require.NoError(t, err)
 
-	ctx := context.Background()
-	defer client.Disconnect(ctx)
+	// Set Timed Connection
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	require.NoError(t, err)
 
+	// Insert Document for test
+	coll := client.Database("brigand").Collection("metadata")
+	doc := storage.Metadata{Id: "none", FileName: "none", CreateAt: time.Now()}
+	_, err = coll.InsertOne(context.TODO(), doc)
+	require.NoError(t, err)
+
+	// Get Document
 	mdc := storage.NewMongoMetadataClient(client)
-
-	// There could or could not be a value?
-	_, err = mdc.GetById(ctx, "magicalid")
+	_, err = mdc.GetById(ctx, "none")
 	require.NoError(t, err)
 
 }
