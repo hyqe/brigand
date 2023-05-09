615c7bbd70c26c85db59b64c329995bdc0c3cf50
LukeOchoa <luke.ochoa@gmail.com>
2023-05-09 19:19:39 +0000

    remove comments

--- internal/storage/metadata_test.go
+++ internal/storage/metadata_test.go
@@ -20,13 +20,11 @@ func Test_mongoMetadataClient_DeleteById_happy_path(t *testing.T) {
 	mongoClient, err := storage.NewMongoClient(ctx, MONGO)
 	defer mongoClient.Disconnect(ctx)
 
-	// Insert a Document to delete for the test
 	coll := mongoClient.Database("brigand").Collection("metadata")
 	doc := storage.Metadata{Id: "none", FileName: "none", CreateAt: time.Now()}
 	_, err = coll.InsertOne(context.TODO(), doc)
 	require.NoError(t, err)
 
-	// Delete the Document
 	mdc := storage.NewMongoMetadataClient(mongoClient)
 	require.NoError(t, mdc.DeleteById(ctx, "none"))
 }
