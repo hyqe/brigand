4f6126446669534372a6e2433bac4cffe7440dbd
LukeOchoa <luke.ochoa@gmail.com>
2023-05-09 18:46:47 +0000

    Change test to use brigand's standard api for opening mongodb client/conn.

--- internal/storage/metadata_test.go
+++ internal/storage/metadata_test.go
@@ -2,38 +2,32 @@ package storage_test
 
 import (
 	"context"
-	"github.com/hyqe/brigand/internal/storage"
-	"github.com/stretchr/testify/require"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/hyqe/brigand/internal/storage"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_mongoMetadataClient_DeleteById_happy_path(t *testing.T) {
 	MONGO, ok := os.LookupEnv("MONGO")
 	if !ok {
-		t.Skipf("Missing env: %s", MONGO)
+		t.Skipf("Missing env: MONGO")
 	}
 
-	// Open Client
-	opts := options.Client().ApplyURI(MONGO)
-	client, err := mongo.NewClient(opts)
-	require.NoError(t, err)
-
-	// Set Timed Connection
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	require.NoError(t, err)
+	ctx := context.Background()
+	mongoClient, err := storage.NewMongoClient(ctx, MONGO)
+	mongoClient.Connect(ctx)
+	defer mongoClient.Disconnect(ctx)
 
 	// Insert a Document to delete for the test
-	coll := client.Database("brigand").Collection("metadata")
+	coll := mongoClient.Database("brigand").Collection("metadata")
 	doc := storage.Metadata{Id: "none", FileName: "none", CreateAt: time.Now()}
 	_, err = coll.InsertOne(context.TODO(), doc)
 	require.NoError(t, err)
 
 	// Delete the Document
-	mdc := storage.NewMongoMetadataClient(client)
+	mdc := storage.NewMongoMetadataClient(mongoClient)
 	require.NoError(t, mdc.DeleteById(ctx, "none"))
 }
