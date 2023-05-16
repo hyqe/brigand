9206a5818bbaecd33102e3b1d535bdbcd9410a05
Jacob Ochoa <jacob.ochoa@gmail.com>
2023-05-16 11:12:34 -0700

    test handler directly, with mocks

--- internal/handlers/get_file_test.go
+++ internal/handlers/get_file_test.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"io"
 
-	"github.com/hyqe/brigand/internal/server"
+	"github.com/google/uuid"
+	"github.com/hyqe/brigand/internal/handlers"
 	"github.com/hyqe/brigand/internal/storage"
 
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 
 	"testing"
@@ -33,36 +35,24 @@ func Test_GetFile_happy_path(t *testing.T) {
 		return nil
 	}
 
-	//mockGetRequestFileId := func(r *http.Request) string {
-	//	return md.Id
-	//}
-	//handler := handlers.NewGetFileById(mockMetadataClient, mockFileDownloader, mockGetRequestFileId)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/ANYTHING GOES", nil)
 
-	srv := httptest.NewServer(server.Routes(mockMetadataClient, mockFileDownloader))
-	defer srv.Close()
+	mockGetFileId := func(r *http.Request) string { return md.Id }
+	handlers.NewGetFileById(mockMetadataClient, mockFileDownloader, mockGetFileId).ServeHTTP(w, r)
 
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%v/files/%v", srv.URL, md.Id), nil)
-	require.NoError(t, err)
-
-	resp, err := http.DefaultClient.Do(req)
-	require.NoError(t, err)
-
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-
-	b, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-
-	require.Equal(t, fileContent, string(b))
+	require.Equal(t, http.StatusOK, w.Code)
+	require.Equal(t, fileContent, w.Body.String())
 }
 
-//func Test_GetFileById_bad_file_id(t *testing.T) {
-//	mock := &storage.MockMetadataClient{}
-//	mockFileDownloader := storage.MockNewS3FileDownloader(fmt.Errorf("NoFileFound"))
-//
-//	w := httptest.NewRecorder()
-//	r := httptest.NewRequest(http.MethodGet, "/files/{fileId}", nil)
-//	r = mux.SetURLVars(r, map[string]string{"fileId": uuid.New().String()})
-//	handlers.NewGetFileById(mock, mockFileDownloader).ServeHTTP(w, r)
-//
-//	require.Equal(t, 404, w.Result().StatusCode)
-//}
+func Test_GetFileById_bad_file_id(t *testing.T) {
+	mock := &storage.MockMetadataClient{}
+	mockFileDownloader := storage.MockNewS3FileDownloader(fmt.Errorf("NoFileFound"))
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/files/{fileId}", nil)
+	r = mux.SetURLVars(r, map[string]string{"fileId": uuid.New().String()})
+	handlers.NewGetFileById(mock, mockFileDownloader).ServeHTTP(w, r)
+
+	require.Equal(t, 404, w.Result().StatusCode)
+}
